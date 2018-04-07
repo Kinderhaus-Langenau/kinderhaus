52afd94d71ccc70b4fa419ead4519b03d21184cf
Thomas Hönig <hoenig.thomas@gmx.de>
2018-04-07 14:17:22 +0200

    Mobile: Dropdown Menü

--- user/themes/kinderhaus/js/kinderhaus.js
+++ user/themes/kinderhaus/js/kinderhaus.js
@@ -1,4 +1,41 @@
 $(document).ready(function() {
   if($('.page-navigation__sub-list:visible').length > 0)
     $('.content__wrapper').addClass('margin-top__large');
+
+  // Mobile Menu setup
+  var $inactiveSubLists = $('.mobile-menu li:not(.active) .page-navigation__sub-list');
+  $inactiveSubLists.each(function(idx, element) {
+    closeSubMenu($(element));
+  });
+
+  // Mobile Pagenavigation dropdown
+  $('.mobile-menu .page-navigation__link').click(function(event) {
+    event.stopPropagation();
+  });
+
+  $('.mobile-menu li').click(function(event) {
+    var $subList = $('.page-navigation__sub-list', this);
+
+    if($subList.hasClass('closed')) {
+      openSubMenu($subList);
+    } else {
+      closeSubMenu($subList);
+    }
+  });
+
+  function closeSubMenu($target) {
+    $target.toggleClass('closed');
+
+    $target.siblings('.page-navigation__dropdown-icon')
+      .removeClass('fa-caret-down')
+      .addClass('fa-caret-left');
+  }
+
+  function openSubMenu($target) {
+    $target.toggleClass('closed');
+
+    $target.siblings('.page-navigation__dropdown-icon')
+      .removeClass('fa-caret-left')
+      .addClass('fa-caret-down');
+  }
 });
