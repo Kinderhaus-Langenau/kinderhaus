116fd75399e58d430f576e0f4de8c57ff94cefd7
Thomas Hönig <hoenig.thomas@gmx.de>
2018-06-21 19:49:08 +0200

    Twig function: polaroid

---
+++ user/plugins/kinderhaus/twig/KinderhausExtension.php
@@ -0,0 +1,38 @@
+<?php
+namespace Grav\Plugin;
+
+use \Grav\Common\Grav;
+
+class KinderhausExtension extends \Twig_Extension
+{
+  public function __construct() {
+    $this->grav = Grav::instance();
+  }
+
+  public function generatePolaroid($img, $title = null, $alignment = null) {
+    switch ($alignment) {
+    case 'mitte':
+      $alignment = 'align-center';
+      break;
+    case 'rechts':
+      $alignment = 'align-right';
+      break;
+    case 'links':
+    default:
+      $alignment = 'align-left';
+    }
+
+    return $this->grav['twig']->processTemplate('partials/polaroid.html.twig',
+                                                ['page' => $this->grav['page'],
+                                                 'image' => $img,
+                                                 'title' => $title,
+                                                 'classes' => $alignment]);
+  }
+
+  public function getFunctions()
+  {
+    return [
+      new \Twig_SimpleFunction('polaroid', [$this, 'generatePolaroid'])
+    ];
+  }
+}
